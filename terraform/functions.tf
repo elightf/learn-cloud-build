7c5d957ff5d6f498dc4fb97327a69beb66b57b04
elightf <90856336+elightf@users.noreply.github.com>
2022-05-27 09:45:39 -0700

    Make buckets destroyable and format

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -12,20 +12,20 @@ resource "google_storage_bucket_object" "zip" {
 
   # Append to the MD5 checksum of the files's content
   # to force the zip to be updated as soon as a change occurs
-  name         = "src-${data.archive_file.source.output_md5}.zip"
-  bucket       = google_storage_bucket.function_bucket.name
+  name   = "src-${data.archive_file.source.output_md5}.zip"
+  bucket = google_storage_bucket.function_bucket.name
 
   # Dependencies are automatically inferred so these lines can be deleted
-  depends_on   = [
-    google_storage_bucket.function_bucket,  # declared in `storage.tf`
+  depends_on = [
+    google_storage_bucket.function_bucket, # declared in `storage.tf`
     data.archive_file.source
   ]
 }
 
 resource "google_cloudfunctions_function" "deployed_function" {
-  name                  = "function-tf" # this forms the endpoint in the url
-  runtime               = "nodejs14"  # of course changeable
-  region                = var.region
+  name    = "function-tf" # this forms the endpoint in the url
+  runtime = "nodejs14"    # of course changeable
+  region  = var.region
 
   # Get the source code of the cloud function as a Zip compression
   source_archive_bucket = google_storage_bucket.function_bucket.name
@@ -33,20 +33,20 @@ resource "google_cloudfunctions_function" "deployed_function" {
   trigger_http          = true
 
   # Must match the functionName in the cloud function source code
-  entry_point           = "hello_tf"
+  entry_point = "hello_tf"
 
   # Dependencies are automatically inferred so these lines can be deleted
-  depends_on            = [
-    google_storage_bucket.function_bucket,  # declared in `storage.tf`
+  depends_on = [
+    google_storage_bucket.function_bucket, # declared in `storage.tf`
     google_storage_bucket_object.zip
   ]
 }
 
 resource "google_cloudfunctions_function_iam_binding" "binding" {
-  project = google_cloudfunctions_function.deployed_function.project
-  region = google_cloudfunctions_function.deployed_function.region
+  project        = google_cloudfunctions_function.deployed_function.project
+  region         = google_cloudfunctions_function.deployed_function.region
   cloud_function = google_cloudfunctions_function.deployed_function.name
-  role = "roles/cloudfunctions.invoker"
+  role           = "roles/cloudfunctions.invoker"
   members = [
     "allUsers",
   ]
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "function_bucket" {
-  name     = "${var.project}-function"
-  location = var.region
+  name          = "${var.project}-function"
+  location      = var.region
+  force_destroy = true
 }

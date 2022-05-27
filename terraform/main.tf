3e702313f1c3f924c7fa4358e1abbe6c79b19570
elightf <90856336+elightf@users.noreply.github.com>
2022-05-27 13:20:05 -0700

    Move main.tf again

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.53"
+    }
+  }
+
+  cloud {
+    organization = "elightf"
+
+    workspaces {
+      name = "learn-cloud-build"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+}
+
+locals {
+  service_folder = "service"
+  service_name   = "ronin"
+
+  ronin_worker_sa = "serviceAccount:${google_service_account.ronin_worker.email}"
+}
+
+output "service_url" {
+  value = "${google_cloud_run_service.ronin.status[0].url}/test"
+}
+
+output "function_url" {
+  value = google_cloudfunctions_function.deployed_function.https_trigger_url
+}
